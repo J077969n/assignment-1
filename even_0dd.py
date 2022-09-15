720749a2bd07cbc2a029c04632281539b717966f
J077969n <112890535+J077969n@users.noreply.github.com>
2022-09-16 01:00:02 +0530

    Add files via upload

---
+++ even_0dd.py
@@ -0,0 +1,16 @@
+
+
+# identifyinf odd and even number from series of numberb
+
+a=0
+j=0
+for i in range(1,10):
+    
+    if (i%2==0):
+      a=a+1
+
+    else :
+        j=j+1
+
+print("number of odd number are : ",j)
+print("number of even number are : ",a)
